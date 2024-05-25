592e00057573001bfe0063c7b690e24af70c41a7
Gabriel Gomes <83957316+rapilukz@users.noreply.github.com>
2024-05-25 17:27:22 +0100

    Init

---
+++ classes/ProducScrapper.py
@@ -0,0 +1,33 @@
+class ProductScraper():
+    def __init__(self, product, storeName, URL):
+        self.product = product
+        self.storeName = storeName
+        self.URL = URL
+        self.df = pd.DataFrame(columns=["name", "category", "price", "store", "ratings", "reviews", "reviews_nr"])
+
+    def to_csv(self, fileName):
+        self.df.to_csv(fileName, index = False)
+        print(f"Data saved successfully to {fileName}")
+
+    @staticmethod
+    def combine_csvs(outputName, *inputFiles):
+        combinedDf = pd.concat((pd.read_csv(f) for f in inputFiles), ignore_index=True)
+        combinedDf.to_csv(outputName, index=False)
+        print(f"All csv files combined into {outputName}")
+
+    def goToNextPage(self):
+        pass
+    
+    def print_details(self):
+        print(f"The product is {self.product}")
+        print(f"The store name is {self.storeName}")
+        print(f"The URL is {self.URL}")
+        # print(self.driver)
+
+    def openPage(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.URL)
+        
+    #Pseudo abstract method xdd
+    def scrape(self):
+        pass
