a67e6d786ca2ce38d6e3ef9a4f22b40680446c45
Bruzn <bryan.teodoro29@gmail.com>
2024-06-03 18:16:11 +0100

    Update pcComponentes.py

--- classes/pcComponentes.py
+++ classes/pcComponentes.py
@@ -1,5 +1,8 @@
-from classes.product_scraper import ProductScraper
 
-class pcComponentes(ProductScraper):
-    def __init__(self, URL):
-      super().__init__("PC Componentes", URL):
+from classes.product_scraper import ProductScraper
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class pcComponentesScrapper(ProductScraper):
+    def __init__(product, self, URL):
+      super().__init__(product, "PC Componentes", "https://www.pccomponentes.pt/")
