485d0d5b70e285d7d83eaf70b05989cd9a296073
Bruzn <bryan.teodoro29@gmail.com>
2024-06-05 21:09:07 +0100

    Update

--- classes/pcComponentes.py
+++ classes/pcComponentes.py
@@ -1,28 +1,57 @@
-
-from classes.product_scraper import ProductScraper
+from time import sleep
+import pandas as pd
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from product_scraper import ProductScraper
 
 class pcComponentesScrapper(ProductScraper):
-    def __init__(product, self, URL):
+    def __init__(self, product):
       super().__init__(product, "PC Componentes", "https://www.pccomponentes.pt/")
       
     def open_browser(self):
       self.openPage()
+      self.close_cookies()
       
     def scrape(self):
       self.open_browser()
       self.search_item()
       
+    def close_cookies(self):
+      sleep(1)
+      print('Closing cookies')
+      try:
+        sleep(1)
+        cookies_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "cookiesAcceptAll"))
+        )
+        cookies_button.click()
+      except:
+        print('No cookies button found')
+        
     def search_item(self):
-      search_bar = self.driver.find_element(By.ID, "search")
-      search_bar.click()
+      search_bar = self.driver.find_element(By.XPATH,'//*[@id="search"]')
       search_bar.send_keys(self.product)
-      search_bar.send_keys(Keys.RETURN)    
+      # sleep(1)
+      # search_bar = WebDriverWait(self.driver, 10).until(
+      #     EC.presence_of_element_located((By.ID, "search"))
+      # )
+      # search_bar.click()
+      # search_bar.send_keys(self.product)
+      # search_bar.send_keys(Keys.RETURN)    
       
+    
+# def main():
+#     x = pcComponentesScrapper("mouse gamer")
+#     # x.open_browser()
+#     # x.search_item()
+#     #time.sleep(1)
+#     x.scrape()
+    
+# Usage example
 if __name__ == "__main__":
-  scraper = pcComponentesScrapper("nvidia geforce rtx 3060 ti")
-  try:
+    scraper = pcComponentesScrapper("ryzen 5 3600")
     scraper.scrape()
-  finally:
-    scraper.close_browser()
--- classes/product_scraper.py
+++ classes/product_scraper.py
@@ -1,15 +1,21 @@
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class ProductScraper():
-    def __init__(self, storeName, URL):
+    def __init__(self, product, storeName, URL):
         self.storeName = storeName
         self.URL = URL
+        self.product = product
         self.df = pd.DataFrame(columns=["name", "category", "price", "store", "ratings", "reviews", "reviews_nr"])
 
     def to_csv(self, fileName):
         self.df.to_csv(fileName, index = False)
         print(f"Data saved successfully to {fileName}")
+
+    def close_browser(self):
+        self.driver.quit()
+        print("Browser closed successfully")
 
     @staticmethod
     def combine_csvs(outputName, *inputFiles):
@@ -19,6 +25,9 @@ class ProductScraper():
 
     def goToNextPage(self):
         pass
+
+    def got_back(self):
+        self.driver.back()
     
     def print_details(self):
         print(f"The store name is {self.storeName}")
@@ -26,9 +35,15 @@ class ProductScraper():
         # print(self.driver)
 
     def openPage(self):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get(self.URL)
-        
+
     #Pseudo abstract method xdd
     def scrape(self):
         pass
+
+    def add_item(self, name, category, price, store, ratings, reviews, reviews_nr):
+        new_row = { "name": name, "category": category, "price": price, "store": store, "ratings": ratings, "reviews": reviews, "reviews_nr": reviews_nr }
+        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_index=True)
