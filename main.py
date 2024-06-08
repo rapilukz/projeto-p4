bdcbe08904bd0941348272cc5d0a4f41364af7ff
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-06-08 21:44:19 +0100

    Added links dict

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from classes.amazon import AmazonScraper
 from classes.worten import WortenScrapper
+from utils.products import links
 
-worten = WortenScrapper("ryzen 5 3600")
-amazon = AmazonScraper("ryzen 5 3600x")
+# worten = WortenScrapper("ryzen 5 3600")
+# amazon = AmazonScraper("ryzen 5 3600x")
 
-worten.scrape()
-amazon.scrape()
+# worten.scrape()
+# amazon.scrape()
 
