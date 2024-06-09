c3fbb07471714fb6f875654c24e4c74672aa68be
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-06-09 02:23:12 +0100

    Refactor: a bit of everything

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from classes.amazon import AmazonScraper
 from classes.worten import WortenScrapper
-from utils.products import links
+from utils.products import products
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-# worten = WortenScrapper("ryzen 5 3600")
-# amazon = AmazonScraper("ryzen 5 3600x")
+## Initialize the webdriver
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
 
-# worten.scrape()
-# amazon.scrape()
+## Initialize the scrapers
+worten = WortenScrapper(driver)
+amazon = AmazonScraper(driver)
 
+## Scrape the products
+driver.get("https://www.google.com")
+for product in products:
+    amazon.scrape_item(products[product]["amazon"])
+    worten.scrape_item(products[product]["worten"])
+
+## Save the data
+# amazon.to_csv("amazon.csv")
+worten.to_csv("worten.csv")
+
+driver.close()
