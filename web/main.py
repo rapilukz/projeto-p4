b3d86e5b36d806142ac671c8571b9606417c1092
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-06-20 19:23:14 +0100

    Added get db

--- web/main.py
+++ web/main.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, g
 import sqlite3
 import os
 
 app = Flask(__name__)
+DATABASE = os.path.join('database', 'products.db')
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection():
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello, World!</p>'
+    return 'Hello, World!'
 
 
 if __name__ == '__main__':
+    if not os.path.exists('database'):
+        os.makedirs('database')
     app.run()
